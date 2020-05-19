e5e14dca444d370b4f81ad70a0b620f1ed024cd2
Ry Sutton <ryancesutton@gmail.com>
2020-05-19 14:36:12 -0500

    set up my first test

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    @Test
+    public void myFirstTest() {
+        String company = "Codeup";
+        assertEquals("Codeup", company);
+    }
+
+}
